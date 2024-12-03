6222c87cb261bba8475aa44c331326492584ddd9
Nicholas Fahrenkrog <2533354+nfahrenk@users.noreply.github.com>
2024-12-02 22:32:22 -0700

    day 3 part 2

--- day3/test.go
+++ day3/test.go
@@ -14,26 +14,32 @@ func main() {
 		return
 	}
 
-	// Convert the byte slice to a string
 	fileContent := string(data)
 
-	r, err := regexp.Compile(`mul\(([0-9]+),([0-9]+)\)`)
+	r, err := regexp.Compile(`don't\(\)|mul\(([0-9]+),([0-9]+)\)|do\(\)`)
 	if err != nil {
 		fmt.Println("could not compile regex", err)
 	}
 	matches := r.FindAllStringSubmatch(fileContent, -1)
 	output := 0
+	enabled := true
 	for _, match := range matches {
 		fmt.Println(match)
-		num1, err := strconv.Atoi(match[1])
-		if err != nil {
-			fmt.Println("could not parse int", err)
+		if match[0] == "don't()" {
+			enabled = false
+		} else if match[0] == "do()" {
+			enabled = true
+		} else if enabled {
+			num1, err := strconv.Atoi(match[1])
+			if err != nil {
+				fmt.Println("could not parse int", err)
+			}
+			num2, err := strconv.Atoi(match[2])
+			if err != nil {
+				fmt.Println("could not parse int", err)
+			}
+			output += num1 * num2
 		}
-		num2, err := strconv.Atoi(match[2])
-		if err != nil {
-			fmt.Println("could not parse int", err)
-		}
-		output += num1 * num2
 	}
 	fmt.Println("results", output)
 }
